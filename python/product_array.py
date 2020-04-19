6652fe2657847a816060f3cdda1831ac61aaf6cd
R-Alex95 <sdi1300147@di.uoa.gr>
2020-04-19 11:24:19 +0300

    leet

--- python/product_array.py
+++ python/product_array.py
@@ -1,27 +1,23 @@
-"""Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
-You are given a target value to search. If found in the array return its index, otherwise return -1.
-You may assume no duplicate exists in the array."""
+""" Print the product of the array except of array[i] without division """
+def array_product(nums):
+    if len(nums) == 1:
+        return nums
 
-def search(nums, target: int) -> int:
-    n = len(nums)
-    if n == 1:
-        if nums[0] == target: 
-            return 0
-        else:
-            return -1
-    low = (n-1)//2
-    high = low + 1 if n%2 == 0 else low
-    while low >= 0 and high < n:
-        if nums[low] == target: return low
-        if nums[high] == target: return high
-        low -= 1
-        high += 1
-    return -1
+    left = [0]*len(nums)
+    right = [0] * len(nums)
+    left[0] = 1
+    right[-1] = 1
+    for i in range(1,len(nums)):
+        left[i] = left[i-1] * nums[i-1] 
+
+    for i in range(len(nums) - 2, -1, -1):
+        right[i] = nums[i+1] * right[i+1]
+    for i in range(len(nums)):
+        right[i] = (right[i] * left[i])
+        
+    return right
 
 if __name__ == "__main__":
-    arr = [4,5,6,7,0,1,3]
-    res1 = search(arr,0)
-    res2 = search(arr,2)
-    assert res1 == 4
-    assert res2 == -1
+    a = [1,2,3,4]
+    p = array_product(a) 
+    print(p)
